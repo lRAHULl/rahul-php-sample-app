68426cc1b50f2a779c4b326f9ef80bff9bd13c38
Rahul P <Rahul.P@viacomcontractor.com>
2021-11-27 09:59:36 +0530

    ADD: initial app code

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>PHP application</title>
+  </head>
+  <body>
+    <h1>Username: <?php echo getenv("ATC_USERNAME"); ?></h1>
+    <h1>Password: <?php echo getenv("ATC_PASSWORD"); ?></h1>
+  </body>
+</html>
