fcf14ef470877898d577701b6f53f0eaca633388
Rahul P <rahulpadmaraju16@gmail.com>
2021-11-30 14:23:49 +0530

    update

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
     <title>PHP application</title>
   </head>
   <body>
-    <h1>Username: <?php echo getenv("ATC_USERNAME"); ?></h1>
-    <h1>Password: <?php echo getenv("ATC_PASSWORD"); ?></h1>
+    <h1>USERNAME: <?php echo getenv("ATC_USERNAME"); ?></h1>
+    <h1>PASSWORD: <?php echo getenv("ATC_PASSWORD"); ?></h1>
   </body>
 </html>
